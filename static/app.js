087d7691adf0c2b3cf642e763fc2d64eb0aace37
The Code Cave Dev <melusi.somkhanda@gmail.com>
2026-08-08 21:55:44 +0200

    implemented ui improvement

--- static/app.js
+++ static/app.js
@@ -1,26 +1,35 @@
 let form = document.getElementById("form");
-let copy_button = document.getElementById("copy_button");
+let link_card = document.getElementById("link-card");
+let copy_button = document.getElementById("copy_button")
 let url = ""
+let url_input = document.getElementById("url")
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    fetch('/shorten',{
-        method:"POST",
-        body: new FormData(this)
-    }).then(response => response.json()
-)
-    .then(data=>{
-        copy_button.style.display = "block";
-        url = data.message
-        document.getElementById('shortly').innerText = data.message;
-    })
+form.addEventListener('submit', function (e) {
+
+    if (url_input.value.trim() !== "") {
+        e.preventDefault()
+        fetch('/shorten', {
+            method: "POST",
+            body: new FormData(this)
+        }).then(response => response.json()
+        )
+            .then(data => {
+                link_card.style.display = "flex";
+                url = data.message
+                document.getElementById('shortly').innerText = data.message;
+            })
+    } else {
+        copy_button.disabled = true
+    }
+
+
 }
 
 )
 
-copy_button.addEventListener("click", function(e){
+copy_button.addEventListener("click", function (e) {
     navigator.clipboard.writeText(url);
-    alert("url copied to clipboard: "+url)
+    alert("url copied to clipboard: " + url)
 }
 
 )
